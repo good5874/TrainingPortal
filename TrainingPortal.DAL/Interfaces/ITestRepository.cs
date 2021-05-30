df4a286b60ffba75daedd5c5569d897e4af91fb2
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-05-30 13:04:39 +0300

    added interfaces

---
+++ TrainingPortal.DAL/Interfaces/ITestRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrainingPortal.Common.Models;
+
+namespace TrainingPortal.DAL.Interfaces
+{
+    public interface ITestRepository : IRepository<Test>
+    {
+    }
+}
---
+++ TrainingPortal.DAL/Interfaces/IСourseRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrainingPortal.Common.Models;
+
+namespace TrainingPortal.DAL.Interfaces
+{
+    public interface IСourseRepository: IRepository<Course>
+    {
+    }
+}
