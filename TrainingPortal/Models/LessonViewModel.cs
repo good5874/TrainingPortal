57fc1474a8af6a694f41be5bc4af9cb82002b4a3
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-06-01 15:09:28 +0300

    added viewModels and views

---
+++ TrainingPortal/Models/LessonViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingPortal.Models
+{
+    public class LessonViewModel
+    {
+        public string NameCourse { get; set; }
+        public string NameLesson { get; set; }
+        public string Material { get; set; }
+    }
+}
---
+++ TrainingPortal/Models/ResultTestViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrainingPortal.BLL.Models;
+
+namespace TrainingPortal.Models
+{
+    public class ResultTestViewModel
+    {
+        public ValidatedTestDTO Test { get; set; }
+        public ValidatedCourseDTO Course { get; set; }
+    }
+}
